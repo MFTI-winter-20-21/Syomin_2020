77b70c39f5f275f5e9db3469ca364bd7beb36662
6weesh9 <artem7in@gmail.com>
2020-12-27 18:17:28 +0300

    Update 3_cinema.py

--- 3_cinema.py
+++ 3_cinema.py
@@ -1,8 +1,13 @@
 user_name = input("Hello, what's your name?")
 print("Hello", user_name)
 user_old = 2020 - int(input("What year were you born?", ))
-print("So you are", user_name, "and you are", user_old, "years")
-if user_old < 18:
-    print(user_name, "we advise you to watch a comedy")
+print( "Please check if the data you entered is correct: So you are", user_name, "and you are", user_old, "years?  If all okey, print '1', elif '0'")
+correctness = int(input())
+if correctness == 1:
+    if user_old < 18:
+        print(user_name, "we advise you to watch a comedy")
+    else:
+        print(user_name, "we advise you to watch a thriller")
 else:
-    print(user_name, "we advise you to watch a thriller")
+    print("Re-enter the data")
+exit("")
