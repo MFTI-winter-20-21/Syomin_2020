0b80f9ed30ed08467136cd5f7275b930f6dd3030
6weesh9 <artem7in@gmail.com>
2020-12-27 18:54:19 +0300

    Update 4_cycle.py

---
+++ 4_cycle.py
@@ -0,0 +1,19 @@
+status = False
+import math
+print("У тебя есть треугольник с катетами 6 и 8, задача: посчитать его гипотенузу.")
+print("Решишь сам или сразу посмотришь решение? 1 - сам, 0 - решение сразу")
+u = int(input())
+if u == 1:
+    print("Вводи ответ")
+if u == 1:
+    while status != True:
+        answer = int(input())
+        if answer == 10:
+            print("Ооо, ты крутой, так держать!")
+            exit("")
+
+        else:
+            print("Нуу, не совсем верно.")
+else:
+    print("А вот и решение: math.sqrt(6**2 + 8**2")
+exit("")
