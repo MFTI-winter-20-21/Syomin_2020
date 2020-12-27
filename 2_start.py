5c6a5e182f72f516a4f9fdac30d5d8701f42e88b
6weesh9 <artem7in@gmail.com>
2020-12-27 15:32:55 +0300

    Create 2_start.py

---
+++ 2_start.py
@@ -0,0 +1,10 @@
+user_name = input("Hello, what's your name?")
+print("Hello", user_name)
+user_old = 2020 - int(input("What year were you born?", ))
+if 4 <= user_old >= 130:
+    exit(" Are u a dino or an alien?")
+if user_old < 18:
+    print("Sorry, but u aren't allowed to be on this site")
+else:
+    country = input("Where are u from?")
+if country = Russia:
